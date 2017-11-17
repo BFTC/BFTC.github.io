b6c7bff405e4ae38fabefd0dd6be17eb93d6cf12
BFTC <33380750+BFTC@users.noreply.github.com>
2017-11-17 12:32:07 -0600

    Update glr_playlist.php

--- glr_playlist.php
+++ glr_playlist.php
@@ -1,3 +1,5 @@
 <?php
-echo file_get_contents("http://205.164.41.50:1976/played.html");
+  echo 'Current PHP version: ' . phpversion();
+  phpinfo();
+  echo file_get_contents("http://205.164.41.50:1976/played.html");
 ?>
